6122fbc8f1b7acf5e12a8db79a18e50fd1d37b41
Paawccio <fordcrown2009@gmail.com>
2023-02-11 04:39:40 +0100

    Finished QR code generator component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import QRCode from 'react-qr-code';
-import './App.css';
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+import TextField from "@mui/material/TextField";
 
-export function GetQrCode(props) {
-    return (
-        <div className="qrcode-area">
-            <QRCode value={props.string} />
-        </div>
-    )
+import QRCode from "qrcode";
+
+import "./App.css";
+
+export function Generator() {
+  const [text, setText] = useState("");
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    QRCode.toCanvas(
+      canvasRef.current,
+      text || "",
+      (error) => error && console.log(error)
+    );
+  }, [text]);
+
+  return (
+    <div>
+      <div className="main--qrcode-area">
+        <canvas ref={canvasRef} />
+      </div>
+
+      <div>
+        <Input typedText={setText}/>
+      </div>
+    </div>
+  );
+}
+
+export function Input(props) {
+  return (
+    <div className="main--input-area">
+      <TextField
+        label="Type something here"
+        id="filled-hidden-label-small"
+        defaultValue=""
+        variant="filled"
+        size="small"
+        sx={{
+          label: { color: "rgb(255 255 255 / 60%)" },
+          input: { color: "white" },
+        }}
+        style={{ width: 300 }}
+        onChange={(e) => props.typedText(e.target.value)}
+      />
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -6,24 +6,43 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./background.css";
 import "./index.css";
 
-import { GetQrCode } from "./App";
+import { Generator } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <ul className="circles">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+
     <div className="container container--background">
       <header>
         <div className="header--title">
           <center>
             <h1>QR Code Generator</h1>
-            <h2>Made with <FavoriteIcon className="icon--heartbeat" style={{ color: 'red' }}/> by Paweł Sumara</h2> 
+            <h2>
+              Made with{" "}
+              <FavoriteIcon
+                className="icon--heartbeat"
+                style={{ color: "red" }}
+              />{" "}
+              by Paweł Sumara
+            </h2>
           </center>
         </div>
       </header>
 
       <main>
-         {/* <GetQrCode string="debug" /> */}
+        <Generator />
       </main>
     </div>
   </React.StrictMode>
